d4cc4085cfecaa8114b996621d4baf9cf67f750c
Mateus Medice <mateus.medice@discover.com.br>
2015-07-07 22:33:34 -0300

    Configuração tomcat + maven + vraptor

---
+++ nutrisys-webapp/src/main/java/br/com/cavy/studios/nutrisys/controller/UserController.java
@@ -0,0 +1,13 @@
+package br.com.cavy.studios.nutrisys.controller;
+
+import br.com.caelum.vraptor.Path;
+
+@Path("/user")
+public class UserController {
+	
+	@Path("/welcome")
+	public void welcome() {
+		
+	}
+	
+}
